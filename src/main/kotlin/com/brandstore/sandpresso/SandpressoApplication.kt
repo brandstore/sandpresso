71b0195242277b76e8f421229bc7f7a8f63852fc
pierre.c <pierre.c@kakaocorp.com>
2019-04-23 22:36:34 +0900

    first commit

---
+++ src/main/kotlin/com/brandstore/sandpresso/SandpressoApplication.kt
@@ -0,0 +1,11 @@
+package com.brandstore.sandpresso
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SandpressoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SandpressoApplication>(*args)
+}
